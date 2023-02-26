ef8e4f25b89c93459f2b69e92943416a99bf7f98
oli-123-ier <75124289+oli-123-ier@users.noreply.github.com>
2023-02-26 19:15:29 +0100

    final comit herpku

--- heroku/settings.py
+++ heroku/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-import django-heroku
-import dj-database-url
+import django_heroku
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -125,8 +125,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = ['static']
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-django-heroku.settings(locals())
+django_heroku.settings(locals())
