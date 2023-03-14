af005cd65453f0401f1a72162eb4f276ae20ca22
oli-123-ier <75124289+oli-123-ier@users.noreply.github.com>
2023-03-14 17:10:19 +0100

    changement setting

--- heroku/settings.py
+++ heroku/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
-import dj_database_url
+# import django_heroku
+# import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kn%!_9(jfud!@8mft36_!@%7*^_(_@1a3x&kwt#jbo-yxl06^t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -78,15 +78,15 @@ WSGI_APPLICATION = 'heroku.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-# 'default': {
-#   'ENGINE': 'django.db.backends.sqlite3',
-#  'NAME': BASE_DIR / 'db.sqlite3',
-# }
-# }
 DATABASES = {
-    'default': dj_database_url.config()
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+# DATABASES = {
+#    'default': dj_database_url.config()
+# }
 
 
 # Password validation
@@ -130,4 +130,4 @@ STATICFILES_DIRS = ['static']
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
